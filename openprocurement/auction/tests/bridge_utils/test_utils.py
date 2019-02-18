c4950e244e6058ee4e69a33a14546cbfe12106cc
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2019-02-18 12:20:27 +0200

    Expand tests

--- openprocurement/auction/tests/bridge_utils/test_utils.py
+++ openprocurement/auction/tests/bridge_utils/test_utils.py
@@ -3,16 +3,18 @@ import mock
 from copy import deepcopy
 from datetime import datetime, timedelta
 
+from iso8601 import parse_date
 
 from openprocurement.auction.bridge_utils.constants import TZ
-from openprocurement.auction.bridge_utils.managers import InsiderAuctionsManager
+from openprocurement.auction.bridge_utils.managers import InsiderAuctionsManager, MANAGERS_MAPPING
 from openprocurement.auction.bridge_utils.utils import (
     planning_auction,
     get_manager_for_auction,
-    check_auction
+    check_auction,
+    check_inner_auction
 )
 from openprocurement.auction.tests.bridge_utils.base import BaseWebTest
-from openprocurement.auction.tests.bridge_utils.data import test_auction_data
+from openprocurement.auction.tests.bridge_utils.data import test_auction_data, plantest
 
 
 test_auction_data_quick = deepcopy(test_auction_data)
@@ -302,3 +304,122 @@ class AuctionPlanning(BaseWebTest):
             start, _, skipped_days = planning_auction(my_test_auction, self.mapper, now, self.db)
 
             self.assertEqual(skipped_days, 2)
+
+
+class TestCheckInnerAuction(BaseWebTest):
+
+    def setUp(self):
+        super(TestCheckInnerAuction, self).setUp()
+        plantest['_id'] = 'plantest_{}'.format(
+            datetime.now().date().isoformat())
+        plantest_from_db = self.db.get(plantest['_id'], {})
+        plantest_from_db.update(plantest)
+
+        self.db.save(plantest_from_db)
+
+    def test_check_inner_auction(self):
+        insider_auction_id = '01fa8a7dc4b8eac3b5820747efc6fe36'
+        texas_auction_id = 'dc3d950743304d05adaa1cd5b0475075'
+        classic_auction_with_lots = 'da8a28ed2bdf73ee1d373e4cadfed4c5'
+        classic_auction_without_lots = 'e51508cddc2c490005eaecb73c006b72'
+        lots_ids = ['1c2fb1e496b317b2b87e197e2332da77',
+                    'b10f9f7f26157ae2f349be8dc2106d6e']
+
+        today = datetime.now().date().isoformat()
+        time = '12:15:00'  # actually, can be any time between 12:00:00 and 12:30:00 due to existing asserts
+        raw_time = ''.join([today, 'T', time])
+
+        # datetime.datetime object prepared in the way scheduler actually does it:
+        test_time = TZ.localize(parse_date(raw_time, None)).isoformat()
+
+        auction = {
+            'id': insider_auction_id,
+            'procurementMethodType': 'dgfInsider',
+            'auctionPeriod': {
+                'startDate': test_time
+            }
+        }
+        mapper = {
+            'pmts': {
+                'dgfInsider': MANAGERS_MAPPING['dutch'](),
+                'landLease': MANAGERS_MAPPING['texas']()
+            },
+            'types': {'english': MANAGERS_MAPPING['english']()}
+        }
+
+        plantest = self.db.get('plantest_{}'.format(today))
+
+        # Test dutch
+        self.assertEqual(len(plantest.get('dutch_streams', [])), 15)
+        self.assertIn(insider_auction_id, plantest.get('dutch_streams'))
+
+        check_inner_auction(self.db, auction, mapper)
+        plantest = self.db.get('plantest_{}'.format(today))
+        self.assertEqual(len(plantest.get('dutch_streams', [])), 6)
+        self.assertNotIn(insider_auction_id, plantest.get('dutch_streams'))
+
+        # Test texas
+        auction['id'] = texas_auction_id
+        auction['procurementMethodType'] = 'landLease'
+
+        self.assertEqual(len(plantest.get('texas_streams', [])), 20)
+        self.assertIn(texas_auction_id, plantest.get('texas_streams'))
+
+        check_inner_auction(self.db, auction, mapper)
+        plantest = self.db.get('plantest_{}'.format(today))
+        self.assertEqual(len(plantest.get('texas_streams', [])), 15)
+        self.assertNotIn(texas_auction_id, plantest.get('texas_streams'))
+
+        # Test classic with lots
+        auction['procurementMethodType'] = 'classic'
+        auction['id'] = classic_auction_with_lots
+        auction['lots'] = [
+            {
+                'id': lots_ids[0],
+                'auctionPeriod': {'startDate': test_time}
+            },
+            {
+                'id': lots_ids[1],
+                'auctionPeriod': {'startDate': test_time}
+            }
+        ]
+        self.assertEqual(len(plantest.get('stream_1')), 10)
+        self.assertEqual(len(plantest.get('stream_2')), 10)
+        stream_1_none_count = len(
+            [v for k, v in plantest.get('stream_1').items() if v is None])
+        stream_2_none_count = len(
+            [v for k, v in plantest.get('stream_2').items() if v is None])
+        self.assertEqual(stream_1_none_count, 0)
+        self.assertEqual(stream_2_none_count, 0)
+        check_inner_auction(self.db, auction, mapper)
+        plantest = self.db.get('plantest_{}'.format(today))
+        self.assertEqual(len(plantest.get('stream_1')), 10)
+        self.assertEqual(len(plantest.get('stream_2')), 10)
+        stream_1_none_count = len(
+            [v for k, v in plantest.get('stream_1').items() if v is None])
+        stream_2_none_count = len(
+            [v for k, v in plantest.get('stream_2').items() if v is None])
+        self.assertEqual(stream_1_none_count, 3)
+        self.assertEqual(stream_2_none_count, 3)
+        self.assertNotIn(classic_auction_with_lots,
+                         plantest.get('stream_1', {}).values())
+        self.assertNotIn(classic_auction_with_lots,
+                         plantest.get('stream_2', {}).values())
+
+        # Test classic without lots
+        del auction['lots']
+        auction['id'] = classic_auction_without_lots
+        check_inner_auction(self.db, auction, mapper)
+        plantest = self.db.get('plantest_{}'.format(today))
+        self.assertEqual(len(plantest.get('stream_1')), 10)
+        self.assertEqual(len(plantest.get('stream_2')), 10)
+        stream_1_none_count = len(
+            [v for k, v in plantest.get('stream_1').items() if v is None])
+        stream_2_none_count = len(
+            [v for k, v in plantest.get('stream_2').items() if v is None])
+        self.assertEqual(stream_1_none_count, 7)
+        self.assertEqual(stream_2_none_count, 6)
+        self.assertNotIn(classic_auction_without_lots,
+                         plantest.get('stream_1', {}).values())
+        self.assertNotIn(classic_auction_without_lots,
+                         plantest.get('stream_2', {}).values())
